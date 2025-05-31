4e80c066b5ebb5f816b3b5260a5792bbb1234bf2
miramiramirami <xeless@mail.ru>
2025-05-31 20:51:38 +0500

    views + urls + catalog and product_list

--- main/models.py
+++ main/models.py
@@ -1,25 +1,29 @@
 from django.db import models
+from django.urls import reverse
+
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, 
-                            db_index=True)
-    slug = models.SlugField(max_length=100,
-                            unique=True)
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=100, unique=True)
+
     class Meta:
         ordering = ('name',)
         verbose_name = 'Категория'
         verbose_name_plural = 'Категории'
-        
+
     def __str__(self):
         return self.name
     
+    def get_absolute_url(self):
+        return reverse("main:product_list_by_category", args=[self.slug])
+    
+    
+
 class Product(models.Model):
     category = models.ForeignKey(Category, related_name='products',
                                  on_delete=models.CASCADE)
-    name = models.CharField(max_length=155,
-                            db_index=True)
-    slug = models.SlugField(max_length=155,
-                            unique=155)
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=100, unique=100)
     image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
     description = models.TextField(blank=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -33,3 +37,5 @@ class Product(models.Model):
     def __str__(self):
         return self.name
     
+    def get_absolute_url(self):
+        return reverse("main:product_detail", args=[self.id, self.slug])
